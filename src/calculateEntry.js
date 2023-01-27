bf3ebb802edf8f0f12d3fb6d6815c12210d51e2f
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-01-27 15:26:37 -0300

    Feat: Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const params = countEntrants(entrants);
+  const amountChild = params.child * data.prices.child;
+  const amountAdult = params.adult * data.prices.adult;
+  const amountSenior = params.senior * data.prices.senior;
+
+  return amountChild + amountAdult + amountSenior;
 };
 
 module.exports = { calculateEntry, countEntrants };
