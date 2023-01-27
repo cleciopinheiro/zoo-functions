6246cee15dcae44af1ea91128361f8fc9ee2c79e
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-01-27 00:53:03 -0300

    Feat: Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const qtdAnimaisResidentes = (animal) => data.species
+  .reduce((sum, spec) => ((spec.name === animal.species)
+    ? (spec.residents) : (sum)), 0).length;
+
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+
+  if (Object.values(animal).length === 1) {
+    return qtdAnimaisResidentes(animal);
+  }
+  return data.species.find((specie) => specie.name === animal.species).residents
+    .reduce((acc, sexo) => ((sexo.sex === animal.sex) ? (acc + 1) : (acc)), 0);
 };
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,5 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...ids) => {
-  const speciesId = data.species.filter((animal) => ids.some((id) => animal.id === id));
-  return speciesId;
-};
+const getSpeciesByIds = (...ids) => data.species.filter((animal) => ids.includes(animal.id));
+
 module.exports = getSpeciesByIds;
