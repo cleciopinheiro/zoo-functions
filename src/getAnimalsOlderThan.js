ea3f42e0ad8386bd3084e46bc337c3ae3fc8f114
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-01-30 10:56:38 -0300

    Fix: refatoramento do requisito 10

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,6 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  const result = data.species.find((specie) => specie.name === animal);
-  return result.residents.every((animale) => animale.age >= age);
-};
+const getAnimalsOlderThan = (animal, age) => data.species.find((specie) => specie.name === animal)
+  .residents.every((animale) => animale.age >= age);
 
 module.exports = getAnimalsOlderThan;
