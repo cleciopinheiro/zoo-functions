38a31d3da58d7103d281b839a15c78199cf27490
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-02-01 14:29:20 -0300

    Feat: Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  // Primeiro passo: fiz o matcher para encontrar o id do meu parametro, já capturando a primeira espécie da chave ResponsibleFor
+  const firstAnimal = data.employees.find((func) => func.id === id).responsibleFor[0];
+  // Segundo passo: Caputrei todos os animais daquela espécie
+  const objectAnimal = data.species.find((specie) => specie.id === firstAnimal).residents;
+  // Terceiro passo: Pra identificar quem é o mais velho, eu organizei a array com 'sort' em ordem crescente e depois fiz um 'length - 1' pra pegar o último dessa lista.
+  return Object.values(objectAnimal.sort((a, b) => a.age - b.age)[objectAnimal.length - 1]);
 };
 
 module.exports = getOldestFromFirstSpecies;
